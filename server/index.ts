fc6a5c4b7f5a1a8628e10c511615b6f0928015ef
Peter Bolfa <xbolfa@fi.muni.cz>
2020-04-24 10:35:57 +0200

    init

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import socketio from 'socket.io';
+import http from 'http';
+
+const PORT : number = 5000; 
+
+const router = require('./router');
+
+const app : express.Application = express();
+const server : http.Server = http.createServer(app);
+const io = socketio(server);
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Started on ${PORT}`));
